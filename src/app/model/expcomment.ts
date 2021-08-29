a19344820dc2a8956922567a0bd9c783b60bf3e5
Mathieu <max94h@yahoo.fr>
2021-08-29 20:28:02 +0200

    guide commentaire lieu detail

--- src/app/model/expcomment.ts
+++ src/app/model/expcomment.ts
@@ -2,7 +2,7 @@ import { Experience } from "./experience";
 
 export class Expcomment {
     id: number=0;
-    text: String="";
+    text: string="";
     rating: number=0;
     date: Date=new Date();
     experience: Experience = new Experience();
--- src/app/model/guide.ts
+++ src/app/model/guide.ts
@@ -2,7 +2,7 @@ export class Guide{
 	id:number = 0;
 	title:string ="";
 	country:string ="";
-	file?:File;
+	images:any;
 	validated:boolean = false;
 	rating:string = "";
 	text:string="";
